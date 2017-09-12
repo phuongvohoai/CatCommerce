39645eec64a472f4299755bfa30c7a18a838bade
phuonghoaivo <phuongvohoai@outlook.com>
2017-09-12 22:08:10 +0700

    Create project + add material

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { MaterialModule } from './material/material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
